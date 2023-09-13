fa7ffc06339f2545c093851ded97a63c9d6e2cd3
Gabriel Lopes <gabrielrrlopes@gmail.com>
2023-09-13 05:37:53 -0400

    Added filter context

---
+++ frontend/src/store/hdbCtx.tsx
@@ -0,0 +1,33 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from "react";
+
+type HdbFilter = {
+  search?: string;
+};
+
+type HdbType = {
+  filter: HdbFilter;
+  setFilter: Dispatch<SetStateAction<HdbFilter>>;
+};
+
+type HdbContextProviderProps = {
+  children: ReactNode;
+};
+
+const HdbContext = createContext<HdbType | null>(null);
+
+const HdbProvider = ({ children }: HdbContextProviderProps) => {
+  const [filter, setFilter] = useState<HdbFilter>({ search: "" });
+  return (
+    <HdbContext.Provider value={{ filter, setFilter }}>
+      {children}
+    </HdbContext.Provider>
+  );
+};
+
+export { HdbContext, HdbProvider };
